206bdeeae65b7656a47fc0b2a42deff5de8e380a
Chris Li <chris.lty07@gmail.com>
2023-10-09 18:09:17 -0400

    add task - convert csv to transactions in db

--- cumulus/api/urls.py
+++ cumulus/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from api.views import SubscriptionListAV, SubscriptionDetailAV, SubTransactionHistoryListAV
+from api.views import SubscriptionListAV, SubscriptionDetailAV, SubTransactionHistoryListAV, ChaseTransactionListAV
 
 urlpatterns = [
     path('list/', SubscriptionListAV.as_view(), name="subscription-list"),
     path('<int:subscription_id>', SubscriptionDetailAV.as_view(), name="subscription-detail"),
-    path('transactions/', SubTransactionHistoryListAV.as_view(), name="transaction-list")
+    path('transactions/', SubTransactionHistoryListAV.as_view(), name="transaction-list"),
+    path('chase_transactions/', ChaseTransactionListAV.as_view(), name="chase-transactions")
+    
 ]
