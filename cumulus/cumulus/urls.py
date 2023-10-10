79cf85bef7e148c1c0844bd57889b5628cde6d75
Chris Li <chris.lty07@gmail.com>
2023-10-09 23:16:59 -0400

    fix root url to cumulus

--- cumulus/cumulus/urls.py
+++ cumulus/cumulus/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('subscriptions/', include('api.urls'))
+    path('cumulus/', include('api.urls'))
 ]
